508b09349239eea0566cb31e4410e11ec54db5ed
Gehannn <gehan_elsayed@hotmail.com>
2020-08-11 16:58:28 +1000

    gfgfdg

--- GhoulsAndGoodies/Intermediate/Build/Win64/UE4Editor/Inc/GhoulsAndGoodies/EnemyAIController.generated.h
+++ GhoulsAndGoodies/Intermediate/Build/Win64/UE4Editor/Inc/GhoulsAndGoodies/EnemyAIController.generated.h
@@ -35,7 +35,7 @@ public: \
 
 #define GhoulsAndGoodies_Source_GhoulsAndGoodies_EnemyAIController_h_15_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
-	NO_API AEnemyAIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	NO_API AEnemyAIController(const FObjectInitializer& ObjectInitializer); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEnemyAIController) \
 	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEnemyAIController); \
 DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEnemyAIController); \
@@ -47,8 +47,6 @@ public:
 
 
 #define GhoulsAndGoodies_Source_GhoulsAndGoodies_EnemyAIController_h_15_ENHANCED_CONSTRUCTORS \
-	/** Standard constructor, called after all reflected properties have been initialized */ \
-	NO_API AEnemyAIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	NO_API AEnemyAIController(AEnemyAIController&&); \
@@ -56,7 +54,7 @@ private: \
 public: \
 	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEnemyAIController); \
 DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEnemyAIController); \
-	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEnemyAIController)
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AEnemyAIController)
 
 
 #define GhoulsAndGoodies_Source_GhoulsAndGoodies_EnemyAIController_h_15_PRIVATE_PROPERTY_OFFSET
