6c0de77aa4ee254731757fe3eb66c9669d0037b6
gtbeazley <50130971+gtbeazley@users.noreply.github.com>
2020-10-28 15:56:06 +1100

    Def Unit resized
    Changed Scale to 2

--- GhoulsAndGoodies/Source/GhoulsAndGoodies/DefendingUnit.cpp
+++ GhoulsAndGoodies/Source/GhoulsAndGoodies/DefendingUnit.cpp
@@ -36,7 +36,7 @@ ADefendingUnit::ADefendingUnit()
 	GetMesh()->SetupAttachment(RootComponent);
 	GetMesh()->SetRelativeRotation(FRotator(0, 0, 0));
 	GetMesh()->SetRelativeLocation(FVector(0, 0, 0));
-	GetMesh()->SetWorldScale3D(FVector(1.5, 1.5, 1.5));
+	GetMesh()->SetWorldScale3D(FVector(2, 2, 2));
 	
 
 	m_curHealth = m_fullHealth;
