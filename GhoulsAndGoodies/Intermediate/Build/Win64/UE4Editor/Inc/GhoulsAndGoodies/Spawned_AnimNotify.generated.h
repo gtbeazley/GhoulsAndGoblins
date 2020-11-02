b3654e84f6ad7c1520c7b448fd1ecc44689d8946
Lily Palmer <55730671+lilybpalmer@users.noreply.github.com>
2020-11-02 16:58:35 +1100

    lighting

---
+++ GhoulsAndGoodies/Intermediate/Build/Win64/UE4Editor/Inc/GhoulsAndGoodies/Spawned_AnimNotify.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef GHOULSANDGOODIES_Spawned_AnimNotify_generated_h
+#error "Spawned_AnimNotify.generated.h already included, missing '#pragma once' in Spawned_AnimNotify.h"
+#endif
+#define GHOULSANDGOODIES_Spawned_AnimNotify_generated_h
+
+#define GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_RPC_WRAPPERS
+#define GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUSpawned_AnimNotify(); \
+	friend struct Z_Construct_UClass_USpawned_AnimNotify_Statics; \
+public: \
+	DECLARE_CLASS(USpawned_AnimNotify, UAnimNotify, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/GhoulsAndGoodies"), NO_API) \
+	DECLARE_SERIALIZER(USpawned_AnimNotify)
+
+
+#define GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesUSpawned_AnimNotify(); \
+	friend struct Z_Construct_UClass_USpawned_AnimNotify_Statics; \
+public: \
+	DECLARE_CLASS(USpawned_AnimNotify, UAnimNotify, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/GhoulsAndGoodies"), NO_API) \
+	DECLARE_SERIALIZER(USpawned_AnimNotify)
+
+
+#define GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USpawned_AnimNotify(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USpawned_AnimNotify) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USpawned_AnimNotify); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USpawned_AnimNotify); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USpawned_AnimNotify(USpawned_AnimNotify&&); \
+	NO_API USpawned_AnimNotify(const USpawned_AnimNotify&); \
+public:
+
+
+#define GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USpawned_AnimNotify(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USpawned_AnimNotify(USpawned_AnimNotify&&); \
+	NO_API USpawned_AnimNotify(const USpawned_AnimNotify&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USpawned_AnimNotify); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USpawned_AnimNotify); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USpawned_AnimNotify)
+
+
+#define GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_PRIVATE_PROPERTY_OFFSET
+#define GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_12_PROLOG
+#define GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_PRIVATE_PROPERTY_OFFSET \
+	GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_RPC_WRAPPERS \
+	GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_INCLASS \
+	GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_PRIVATE_PROPERTY_OFFSET \
+	GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_INCLASS_NO_PURE_DECLS \
+	GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> GHOULSANDGOODIES_API UClass* StaticClass<class USpawned_AnimNotify>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID GhoulsAndGoodies_Source_GhoulsAndGoodies_Public_Spawned_AnimNotify_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
