358905ea9c5e19654d4319e7f6d85cdc4f7d7f02
gtbeazley <50130971+gtbeazley@users.noreply.github.com>
2020-11-02 15:59:21 +1100

    Tile Highights Changed

--- GhoulsAndGoodies/Intermediate/Build/Win64/GhoulsAndGoodiesEditor/Development/Engine/SharedPCH.Engine.cpp
+++ GhoulsAndGoodies/Intermediate/Build/Win64/GhoulsAndGoodiesEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "D:/Documents/GitHub/GhoulsAndGoblins/GhoulsAndGoodies/Intermediate/Build/Win64/GhoulsAndGoodiesEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/s181813/Documents/GitHub/GoodiesAndGhouls/GhoulsAndGoodies/Intermediate/Build/Win64/GhoulsAndGoodiesEditor/Development/Engine/SharedPCH.Engine.h"
--- GhoulsAndGoodies/Intermediate/Build/Win64/UE4Editor/Inc/GhoulsAndGoodies/GhoulsAndGoodies.init.gen.cpp
+++ GhoulsAndGoodies/Intermediate/Build/Win64/UE4Editor/Inc/GhoulsAndGoodies/GhoulsAndGoodies.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeGhoulsAndGoodies_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x2182899F,
-				0xFB640050,
+				0xE65BFD45,
+				0x6C7CDF30,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
